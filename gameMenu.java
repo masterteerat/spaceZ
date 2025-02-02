608efda264d70f0c206d4830156c95e5611c054b
peak2548 <peakpairch@gmaill.com>
2025-02-02 21:59:15 +0700

    Test

---
+++ gameMenu.java
@@ -0,0 +1,39 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
+
+public class gameMenu {
+    JFrame frame;
+    JLabel highScore;
+    JLabel stage;
+    JButton start;
+    JLabel gameName;
+    JLabel credit;
+
+    public gameMenu() {
+        frame = new JFrame("SPACEZ");
+        frame.setLayout(null);
+        frame.setResizable(false);
+        frame.setSize(720,1000);
+        frame.getContentPane().setBackground(Color.BLACK);
+
+        setElement();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    private void setElement() {
+        gameName = new JLabel("SPACEZ");
+        gameName.setBounds(130,100,600,70);
+        gameName.setFont(new Font("SpaceX", Font.BOLD, 72));
+        gameName.setForeground(new Color(0, 255, 255));
+
+        start = new JButton("Start!!!!");
+        start.setBounds(280,400,150,50);
+        start.setBackground(new Color(255, 102, 0)); start.setForeground(new Color(0, 0, 0));
+
+        frame.add(gameName); frame.add(start);
+    }
+}

811b7e6e8600646005355cd653932d843bb119e6
peak2548 <peakpairch@gmaill.com>
2025-02-02 20:55:29 +0700

    Beta test

--- GUI.java
+++ GUI.java
@@ -1,17 +1,24 @@
 import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
 
 public class GUI {
     public static void main(String[] args) {
         JFrame frame = new JFrame("SPACEZ");
         frame.setLayout(null);
-        frame.setSize(1280,720);
+        frame.setResizable(false);
+        frame.setSize(720,1000);
+        frame.getContentPane().setBackground(Color.BLACK);
 
-        JButton button = new JButton("Test");
-        button.setBounds(100,200,600,50);
+        JLabel gameName = new JLabel("SPACEZ");
+        gameName.setBounds(210,100,300,70);
+        gameName.setFont(new Font("Arial", Font.BOLD, 72));
 
-        frame.add(button);
+        JButton button = new JButton("Start!!!!");
+        button.setBounds(280,400,150,50);
+
+        frame.add(button); frame.add(gameName);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-
     }
 }
